d2c66901f55805023e0f2f195efbda0614c11ffa
Jessenia Guapisaca <jessenia.guapisaca@f1006-19-34183.bmcc.cuny.edu>
2018-02-07 19:13:45 -0500

    new files

---
+++ input/app.js
@@ -0,0 +1,11 @@
+const submitButton = document.getElementById("submit");
+const nameInput = document.getElementById("name");
+const bioInput = document.getElementById("bio");
+const boroughSelect = document.getElementById("borough");
+
+submitButton.onclick = function(event) {
+    const message = document.createElement("p");
+    message.textContent = "Hello," + nameInput.value + " from " + borough.value + ", Welcome to my Website.";
+    
+    document.body.appendChild(message);
+};
